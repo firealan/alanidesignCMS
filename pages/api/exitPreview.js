82077628702e1bcedd0c7ddd56a5e99d8160e08f
alan <alanidesign@gmail.com>
2021-03-12 17:53:55 -0500

    Revert "Initial commit"
    This reverts commit 926a50febe9ce3d55990d6c71084a299b3cea95c.

--- pages/api/exitPreview.js
+++
@@ -1,9 +0,0 @@
-export default async (req, res) => {
-	// Clears the preview mode cookies.
-	// This function accepts no arguments.
-	res.clearPreviewData()
-
-	// Redirect to the slug
-	res.writeHead(307, { Location: req.query.slug })
-	res.end()
-}
